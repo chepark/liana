581c6914fbc3f6537eadf12d3d2d58a6d643be6a
Chaeah Park <102865440+chepark@users.noreply.github.com>
2022-12-20 14:25:01 +0200

    Added styles and html structure.

---
+++ scripts/main.js
@@ -0,0 +1,28 @@
+const mainContent = document.querySelector(".main");
+const navCloseBtn = document.getElementById("nav-close");
+const navOpenBtn = document.getElementById("nav-open");
+const navModal = document.querySelector(".responsive-navigation");
+
+function openNavModal() {
+  navModal.style.display = "block";
+  document.body.style.overflowY = "hidden";
+  // ? why this code? remove?
+  navOpenBtn.dataset.toggle = "on";
+}
+
+function closeNavModal() {
+  navModal.style.display = "none";
+  // ? why this code? remove?
+  navOpenBtn.dataset.toggle = "off";
+  document.body.style.overflowY = "scroll";
+}
+
+// open reponsive navigator
+navOpenBtn.addEventListener("click", () => {
+  openNavModal();
+});
+
+// close responsive navigator
+navCloseBtn.addEventListener("click", () => {
+  closeNavModal();
+});
